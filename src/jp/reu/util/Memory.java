5b20eeca1d3f89ca285fb1e4e899b5e3166427ee
1107AND <1107AND@6A-05>
2014-12-22 17:23:39 +0900

    memmory

--- src/jp/reu/util/Memory.java
+++ src/jp/reu/util/Memory.java
@@ -1,72 +1,72 @@
 package jp.reu.util;
 
-import java.awt.Point;
+import jp.reu.util.diagram.Point;
 
 public class Memory
 {
 	//// singleton ////
-	
+
 	private static Memory instance;
 	public static Memory getInstance()
 	{
 		if (instance == null) {
 			instance = new Memory();
 		}
-		
+
 		return instance;
 	}
 	////
-	
+
 	private Runtime r;
 
 	private Memory () {
-		r = Runtime.getRuntime();
+		this.r = Runtime.getRuntime();
 	}
-	
+
 	public long getTotal ()
 	{
-		return r.totalMemory();
+		return this.r.totalMemory();
 	}
-	
+
 	public long getMax ()
 	{
-		return r.maxMemory();
+		return this.r.maxMemory();
 	}
 
 	public long getFree()
 	{
-		return r.freeMemory();
+		return this.r.freeMemory();
 	}
-	
+
 	public long getUsed ()
 	{
-		return r.totalMemory() - getFree();
+		return this.r.totalMemory() - this.getFree();
 	}
-	
+
 	public long getTotalFree ()
 	{
-		return getMax() - getUsed();
+		return this.getMax() - this.getUsed();
 	}
-	
+
 	public void desGC()
 	{
-		r.gc();
+		this.r.gc();
 	}
 
 	@Override
 	public String toString()
 	{
-		return String.format("MEMORY used / total: %d / %d (%.2f%%) ", 
-				getUsed(), getTotal(),
-				(float)getUsed() * 100f / (float)getTotal());
+		return String.format("MEMORY used / total: %d / %d (%.2f%%) ",
+				this.getUsed(), this.getTotal(),
+				this.getUsed() * 100f / this.getTotal());
 	}
-	
+
 	public void print()
 	{
 		System.out.println(this);
 	}
-	
-	
+
+
 
     public static void main(String[] args) {
     	class Chain
@@ -74,29 +74,29 @@ public class Memory
     		Chain next;
     		Point p;
     	}
-    	
+
     	Chain c1;
-    	
+
     	Memory.getInstance().print();
-    	
+
     	c1 = new Chain();
-    	
+
     	Memory.getInstance().print();
-    	
-    	for (int i = 0; i < 1000; i ++) 
+
+    	for (int i = 0; i < 1000; i ++)
     	{
     		c1.p = new Point(i, i * i);
     		c1.next = new Chain();
-    		
+
     		c1 = c1.next;
-	
+
     		Memory.getInstance().desGC();
     		Memory.getInstance().print();
     	}
-    	
+
 //    	c1 = null;
 //    	c2 = null;
-//    	
+//
 //    	Memory.getInstance().print();
     }
 }
